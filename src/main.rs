835e3192e825f6ea8983eec5212c76bd4a28cc78
Bill Armstrong <armsty9@gmail.com>
2023-06-21 19:07:38 +0300

    Able to get latest block number and txns it contains

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use dotenv::dotenv;
+use solana_client::{rpc_client::RpcClient, rpc_config::RpcBlockConfig};
+use solana_transaction_status::{EncodedConfirmedBlock, UiTransactionEncoding};
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    env_logger::init();
+
+    log::info!("Solana count transactions per second!");
+
+    let client = RpcClient::new("https://solana-api.projectserum.com");
+
+    let solana_version = client.get_version().unwrap().solana_core;
+    log::info!("Solana version: {}", &solana_version);
+
+    let latest_block_number = client.get_slot().unwrap();
+    let block = get_block(&client, latest_block_number);
+    log::info!("Transactions count: {}", block.transactions.len());
+}
+
+//Function thhat retrieves block object based on a given block number
+fn get_block(client: &RpcClient, block_num: u64) -> EncodedConfirmedBlock {
+    log::debug!("Getting block number: {}", block_num);
+
+    let config = RpcBlockConfig {
+        encoding: Some(UiTransactionEncoding::Base64),
+        max_supported_transaction_version: Some(0),
+        ..Default::default()
+    };
+
+    let block = client.get_block_with_config(block_num, config);
+    let encoded_block: EncodedConfirmedBlock = block.unwrap().into();
+
+    encoded_block
 }
